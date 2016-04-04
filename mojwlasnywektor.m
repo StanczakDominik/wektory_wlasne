3e1ffd5f1299f06ad86694cbb51344f17068d7d5
tildekara <karnacz@o2.pl>
2016-04-04 19:37:10 +0200

    Teraz chyba działa porządnie!
    Bo dodałam sobie sorawdzanie rozmiaru macierzy i dla każdej powinno
    zadziałać

--- mojwlasnywektor.m
+++ mojwlasnywektor.m
@@ -1,20 +1,25 @@
 a = [4, 1, 0; 0, 2, 1; 0, 0, -1]
 x = [1; 1; 1]
-dokladnosc = 0.05
+dokladnosc = 0.01
 
 temp = a*x
 lambda = temp(1,1)
 x = temp/temp(1,1)
+N = size(temp)
+n = N(1,1)
 
-while abs(temp(2,1)) >= dokladnosc
-  while abs(temp(3,1)) >= dokladnosc
-    temp = a*x
-    lambda = [lambda temp(1,1)]
-    x = temp/temp(1,1)
+for i = 2:1:n
+  while abs(temp(i,1)) >= dokladnosc
+    while abs(temp(i,1)) >= dokladnosc
+      temp = a*x
+      lambda = [lambda temp(1,1)]
+      x = temp/temp(1,1)
+    end
   end
-end
+end 
 
 disp(x)
 disp(lambda)
-
+printf('wartosc wlasna: ')
+disp(lambda(1,end))
 
